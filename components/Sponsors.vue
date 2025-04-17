f1eee5590c881704b71c26e04e7a1d73e67e22c1
trent-fisher <trent@tailorfittech.com>
2025-04-17 16:17:22 +0200

    fixed sponser logos

--- components/Sponsors.vue
+++ components/Sponsors.vue
@@ -13,7 +13,6 @@
         <Carousel :value="sponsors" :numVisible="3" :numScroll="1" :responsiveOptions="responsiveOptions" :showIndicators="false" circular :autoplayInterval="3000">
           <template #item="slotProps">
             <div class="flex justify-center">
-              <!-- <div class="bg-white p-6 border-green-500"> -->
                 <img :src="slotProps.data.image" :alt="slotProps.data.name" class="bg-white mb-4 rounded-xl w-40 md:w-48 h-32 md:h-48" />
                 <!-- <div class="font-semibold text-white text-sm md:text-xl">{{ slotProps.data.name }}</div> -->
             </div>
