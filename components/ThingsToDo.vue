a063112e03f6a9d6ec2b1f864ba20106156689cb
remanotft <remano@tailorfittech.com>
2025-04-18 20:10:52 +0200

    Fixed Things To Do Carousel

--- components/ThingsToDo.vue
+++ components/ThingsToDo.vue
@@ -3,9 +3,6 @@
 		<h1 class="font-extrabold md:font-extrabold md:text-3xl">THINGS TO DO</h1>
 		<div class="underline"></div>
 
-		
-
-
 		<!-- Tab Selector -->
 		<div class="flex justify-center pt-2">
 			<div class="flex rounded-lg md:w-[30rem] text-sm todo-tab-selector">
@@ -21,82 +18,158 @@
 			</div>
 		</div>
 
-		<!-- <div class="tab-selector-container">
-			<div class="md:w-[30rem] text-white text-sm tab-selector">
-				<div v-for="(item, index) in categories" :key="index" @click="selected = index"
-					class="px-2 py-2 text-center cursor-pointer">
-					<div class="font-semibold">
-						<span :class="selected === index ? 'text-green-500' : 'text-white'">
-							{{ item.name.toUpperCase() }}
-						</span>
-					</div>
-				</div>
-			</div>
-		</div> 
-
-
-		<div class="flex justify-center">
-			<div class="flex border border-black rounded-full md:w-[30rem] overflow-hidden text-sm">
-				<div v-for="(item, index) in categories" :key="index" @click="selected = index" :class="[
-					'flex-1 text-center px-4 py-2 font-semibold cursor-pointer transition-all duration-300',
-					selected === index ? 'bg-black text-green-500' : 'bg-white text-black',
-					index !== categories.length - 1 ? 'border-r border-black' : '',
-					index === 0 ? 'rounded-l-full' : '',
-					index === categories.length - 1 ? 'rounded-r-full' : ''
-				]">
-					{{ item.name.toUpperCase() }}
-				</div>
-			</div>
-		</div> -->
-
 		<!-- Carousel -->
-		<Carousel :value="filteredThings" :numVisible="3" circular :responsiveOptions="responsiveOptions"
-			:showIndicators="false" :autoplayInterval="3000">
-			<template #item="slotProps">
-				<div class="p-2 md:p-5">
-					<div class="flex flex-col bg-white shadow-md rounded-lg overflow-hidden">
-						<!-- Image -->
-						<div class="w-full">
-							<img :src="slotProps.data.imageUrl" :alt="slotProps.data.name"
-								class="rounded-t-lg w-full h-48 md:h-72 object-cover" />
-						</div>
+		<div v-if="selected === 0">
+			<Carousel :data-key="filteredThings.id" :value="filteredThings" :numVisible="3" :numScroll="3" circular :autoplayInterval="3000"
+				:responsiveOptions="responsiveOptions">
+				<template #item="slotProps">
+					<div class="p-2 md:p-5">
+						<div class="flex flex-col bg-white shadow-md rounded-lg overflow-hidden">
+							<!-- Image -->
+							<div class="w-full">
+								<img :src="slotProps.data.imageUrl" :alt="slotProps.data.name"
+									class="rounded-t-lg w-full h-48 md:h-72 object-cover" />
+							</div>
 
-						<!-- Content -->
-						<div class="flex flex-col justify-between space-y-2 p-3">
-							<div>
-								<!-- Name -->
-								<div class="items-center font-medium md:text-lg text-2xl">
-									{{ slotProps.data.name }}
-								</div>
-
-								<!-- Contact Info -->
-								<div class="space-y-1 mt-2 text-gray-700 text-sm md:text-base">
-									<div class="font-semibold">
-										<i class="p-2 text-gray-600 text-base pi pi-envelope"></i>
-										<a :href="`mailto:${slotProps.data.email}`" class="hover:underline">
-											{{ slotProps.data.email }}
-										</a>
+							<!-- Content -->
+							<div class="flex flex-col justify-between space-y-2 p-3">
+								<div>
+									<!-- Name -->
+									<div class="items-center font-medium md:text-lg text-2xl">
+										{{ slotProps.data.name }}
 									</div>
-									<div class="flex justify-between font-semibold">
-										<div>
-											<i class="p-2 text-gray-600 text-base pi pi-phone"></i>
-											<a :href="`tel:${slotProps.data.phoneNo}`" class="hover:underline">
-												{{ slotProps.data.phoneNo }}
+
+									<!-- Contact Info -->
+									<div class="space-y-1 mt-2 text-gray-700 text-sm md:text-base">
+										<div class="font-semibold">
+											<i class="p-2 text-gray-600 text-base pi pi-envelope"></i>
+											<a :href="`mailto:${slotProps.data.email}`" class="hover:underline">
+												{{ slotProps.data.email }}
 											</a>
 										</div>
-										<div class="pt-2">
-											<a :href="slotProps.data.website" target="_blank" rel="noopener noreferrer">
-												<i class="text-blue-500 text-sm pi pi-external-link"></i>
-											</a>
+										<div class="flex justify-between font-semibold">
+											<div>
+												<i class="p-2 text-gray-600 text-base pi pi-phone"></i>
+												<a :href="`tel:${slotProps.data.phoneNo}`" class="hover:underline">
+													{{ slotProps.data.phoneNo }}
+												</a>
+											</div>
+											<div class="pt-2">
+												<a :href="slotProps.data.website" target="_blank"
+													rel="noopener noreferrer">
+													<i class="text-blue-500 text-sm pi pi-external-link"></i>
+												</a>
+											</div>
 										</div>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			</template>
-		</Carousel>
+				</template>
+			</Carousel>
+		</div>
+		<div v-if="selected === 1">
+			<Carousel :data-key="filteredThings.id" :value="filteredThings" :numVisible="3" :numScroll="3" circular :autoplayInterval="3000"
+				:responsiveOptions="responsiveOptions">
+				<template #item="slotProps">
+					<div class="p-2 md:p-5">
+						<div class="flex flex-col bg-white shadow-md rounded-lg overflow-hidden">
+							<!-- Image -->
+							<div class="w-full">
+								<img :src="slotProps.data.imageUrl" :alt="slotProps.data.name"
+									class="rounded-t-lg w-full h-48 md:h-72 object-cover" />
+							</div>
+
+							<!-- Content -->
+							<div class="flex flex-col justify-between space-y-2 p-3">
+								<div>
+									<!-- Name -->
+									<div class="items-center font-medium md:text-lg text-2xl">
+										{{ slotProps.data.name }}
+									</div>
+
+									<!-- Contact Info -->
+									<div class="space-y-1 mt-2 text-gray-700 text-sm md:text-base">
+										<div class="font-semibold">
+											<i class="p-2 text-gray-600 text-base pi pi-envelope"></i>
+											<a :href="`mailto:${slotProps.data.email}`" class="hover:underline">
+												{{ slotProps.data.email }}
+											</a>
+										</div>
+										<div class="flex justify-between font-semibold">
+											<div>
+												<i class="p-2 text-gray-600 text-base pi pi-phone"></i>
+												<a :href="`tel:${slotProps.data.phoneNo}`" class="hover:underline">
+													{{ slotProps.data.phoneNo }}
+												</a>
+											</div>
+											<div class="pt-2">
+												<a :href="slotProps.data.website" target="_blank"
+													rel="noopener noreferrer">
+													<i class="text-blue-500 text-sm pi pi-external-link"></i>
+												</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</template>
+			</Carousel>
+		</div>
+		<div v-if="selected === 2">
+			<Carousel :data-key="filteredThings.id" :value="filteredThings" :numVisible="3" :numScroll="3" circular :autoplayInterval="3000"
+				:responsiveOptions="responsiveOptions">
+				<template #item="slotProps">
+					<div class="p-2 md:p-5">
+						<div class="flex flex-col bg-white shadow-md rounded-lg overflow-hidden">
+							<!-- Image -->
+							<div class="w-full">
+								<img :src="slotProps.data.imageUrl" :alt="slotProps.data.name"
+									class="rounded-t-lg w-full h-48 md:h-72 object-cover" />
+							</div>
+
+							<!-- Content -->
+							<div class="flex flex-col justify-between space-y-2 p-3">
+								<div>
+									<!-- Name -->
+									<div class="items-center font-medium md:text-lg text-2xl">
+										{{ slotProps.data.name }}
+									</div>
+
+									<!-- Contact Info -->
+									<div class="space-y-1 mt-2 text-gray-700 text-sm md:text-base">
+										<div class="font-semibold">
+											<i class="p-2 text-gray-600 text-base pi pi-envelope"></i>
+											<a :href="`mailto:${slotProps.data.email}`" class="hover:underline">
+												{{ slotProps.data.email }}
+											</a>
+										</div>
+										<div class="flex justify-between font-semibold">
+											<div>
+												<i class="p-2 text-gray-600 text-base pi pi-phone"></i>
+												<a :href="`tel:${slotProps.data.phoneNo}`" class="hover:underline">
+													{{ slotProps.data.phoneNo }}
+												</a>
+											</div>
+											<div class="pt-2">
+												<a :href="slotProps.data.website" target="_blank"
+													rel="noopener noreferrer">
+													<i class="text-blue-500 text-sm pi pi-external-link"></i>
+												</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</template>
+			</Carousel>
+		</div>
+
 	</div>
 </template>
 
@@ -132,7 +205,7 @@ const filteredThings = computed(() => {
 const responsiveOptions = ref([
 	{
 		breakpoint: '1400px',
-		numVisible: 2,
+		numVisible: 4,
 		numScroll: 1,
 	},
 	{
